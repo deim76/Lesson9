dd9c4f1179a0105e7d29c655a199e1ba599dcc04
deim76 <53413337+deim76@users.noreply.github.com>
2019-10-07 11:24:30 +1000

    Update task3_2.sql

--- task3_2.sql
+++ task3_2.sql
@@ -3,30 +3,7 @@
 # Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены.
 # При попытке присвоить полям NULL-значение необходимо отменить операцию.
 
-/*DROP TABLE IF EXISTS products;
-CREATE TABLE products (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Название',
-  desription TEXT COMMENT 'Описание',
-  price DECIMAL (11,2) COMMENT 'Цена',
-  catalog_id INT UNSIGNED,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  KEY index_of_catalog_id (catalog_id)
-) COMMENT = 'Товарные позиции';
-
-INSERT INTO products
-  (name, desription, price, catalog_id)
-VALUES
-  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
-  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
-  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
-  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
-  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
-  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
-  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);*/
-
-/*CREATE TRIGGER check_catalog_id_insert BEFORE INSERT ON products
+CREATE TRIGGER check_catalog_id_insert BEFORE INSERT ON products
 FOR EACH ROW
 BEGIN
   IF isnull(NEW.name) and isnull(NEW.desription) THEN
